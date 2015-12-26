ce1f8c7a2da8e0f7ba01b4d6f2e530ca6d09d371
gigatune <gigatune@users.noreply.github.com>
2015-12-26 16:12:23 +0900

    Initial Commit

---
+++ ThrowThePointerToMenubar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ThrowThePointerToMenubar
+//
+//  Created by gigatune on 2015/12/26.
+//  Copyright © 2015年 gigatune. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
