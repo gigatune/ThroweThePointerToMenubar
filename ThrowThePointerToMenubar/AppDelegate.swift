d22692ccefa50867b739f198bff466d5d112c287
gigatune <gigatune@users.noreply.github.com>
2015-12-26 16:57:47 +0900

    ステータスバーと終了関数を追加

--- ThrowThePointerToMenubar/AppDelegate.swift
+++ ThrowThePointerToMenubar/AppDelegate.swift
@@ -11,16 +11,36 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        setMenu()
+    
     }
 
+    func setMenu(){
+        self.statusItem.title = "ThrowThePointerToMenuBar"
+        self.statusItem.highlightMode = true
+
+        let menu = NSMenu()
+        self.statusItem.menu = menu
+
+        
+        let menuItem = NSMenuItem()
+        menuItem.title = "Quit"
+        menuItem.action = Selector("quit:")
+
+        menu.addItem(menuItem)
+    }
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
+    @IBAction func quit(sender:NSButton){
+        NSApplication.sharedApplication().terminate(self)
+    }
 
 }
 
