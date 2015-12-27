76461469895b7c6f111c89d33825f6d21b67e6bf
gigatune <gigatune@users.noreply.github.com>
2015-12-27 16:50:52 +0900

    ショートカットキーでマウスカーソルの移動を実装

--- ThrowThePointerToMenubar/AppDelegate.swift
+++ ThrowThePointerToMenubar/AppDelegate.swift
@@ -7,18 +7,53 @@
 //
 
 import Cocoa
+import Foundation
+import AppKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
-
+    var isThrowed = false
+    let jumpKeyCode = UInt16(7)  // x
+    let backKeyCode = UInt16(40) // k
+    var orgPositon = CGPointMake(0, 0)
+    let menuMousePositon = CGPoint(x: 100, y: 10)
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
         setMenu()
-    
+        setWatcher()
     }
 
+
+    func setWatcher(){
+        NSEvent.addGlobalMonitorForEventsMatchingMask(NSEventMask.KeyDownMask, handler:self.handler)
+    }
+    
+    func handler( event: NSEvent!){
+        if( event.keyCode == self.jumpKeyCode ){
+            if( event.modifierFlags.contains( [.CommandKeyMask, .ShiftKeyMask] ) ){
+                self.gotoMenuPosition()
+            }
+        }
+        if( event.keyCode == self.backKeyCode ){
+            if( event.modifierFlags.contains( [.CommandKeyMask, .ShiftKeyMask] ) ){
+                self.backToMousePositon()
+            }
+        }
+    }
+    
+    func gotoMenuPosition(){
+        let event = CGEventCreate( nil )
+        self.orgPositon = CGEventGetLocation(event)
+        CGWarpMouseCursorPosition( self.menuMousePositon )
+    }
+
+    func backToMousePositon(){
+        CGWarpMouseCursorPosition(self.orgPositon)
+    }
+    
     func setMenu(){
         self.statusItem.title = "ThrowThePointerToMenuBar"
         self.statusItem.highlightMode = true
